3782a73d60e52364b81b6feccac4d51c0eed175c
bangalia <anitabangali@yahoo.com>
2022-03-04 18:33:20 -0600

    Finished
    Had to keep remaking repositories, excess projects caught in cache

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Heading from './Heading';
+import MetalMeta from './MetalMeta';
+import BandList from './BandList';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <MetalMeta />
+      <BandList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Heading.css"
+
+function Heading() {
+    return (
+      <div className="Heading">
+        <h1>ACS 1320 Final Assessment</h1>
+        <div className="Name">Anita Bangali</div>
+      </div>
+    )
+  }
+  
+  export default Heading
+  
